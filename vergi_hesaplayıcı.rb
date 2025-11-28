362ee2d789c91c4806136fbd0232149e94824fca
Arda Aras <ardapatu@gmail.com>
2025-11-28 14:52:38 +0300

    Kaynak kod

---
+++ vergi_hesaplayıcı.rb
@@ -0,0 +1,6 @@
+total = 0
+printf "Satın alım yapılan ürünün vergisiz değerini giriniz:"
+item_value = gets.chomp.to_i
+total = item_value * 1.18
+printf "Ödenmesi gereken KDV dahil yaklaşık tutar:"
+puts total
